47d4f576680237a84c35f0067fa60f98a795e81f
Nikofoxxx <nicolashenrique2@hotmail.com>
2014-05-05 19:45:21 -0300

    Update 6

---
+++ trabalho lab 5/scripts/login.js
@@ -0,0 +1,8 @@
+// var nome= só teste;
+//Comitado pelo tião em 05/05/2014
+function validar(){
+	// alert("bem vindo" +nome+"!")
+	// window.close('this');
+	window.open('pagina_inicial/pagina_inicial.html','_parent');
+	// document.location="pagina_inicial/pagina_inicial.html";
+}
