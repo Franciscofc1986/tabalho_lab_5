2e0e47cc9a12be372f9de8c51f5d703a329aacf9
Nikofoxxx <nicolashenrique2@hotmail.com>
2014-05-10 15:21:34 -0300

    Update 11

---
+++ trabalho lab 5/pagina_funcionarios/js/script.js
@@ -0,0 +1,34 @@
+function pagina_principal(){
+	window.location = '../pagina_inicial/pagina_inicial.html';	
+}
+function empresas(){
+	window.location = '../pagina_empresas/index.html';
+}
+function funcionarios(){
+	window.location = '../pagina_funcionarios/index.html';
+}
+function folha(){
+	window.location = '../pagina_folha/index.html';
+}
+function relatorio(){
+	alert('não implementado(ainda!)')
+}
+function suporte(){
+	alert(' FNS Developers emails de contato:\nnicolashenrique2@hotmail.com\nfrancielegarcia38@yahoo.com.br\nsbaneto@yahoo.com.br')
+}
+function login(){
+	if(confirm('Você tem certeza que quer sair do sistema?'))
+		window.location = '../index.html';
+	//else (alert('OK, esta janela não será fechada ainda.'))
+}
+/*
+<a href="javascript:void();" onclick="login()" id ="item7" class="top_link"><span class="down">Logout</span></a>
+			<ul class="sub">
+			<li><a href="javascript:void();" onclick="fechar()">Fechar</a></li>	
+
+
+function fechar(){
+	if(confirm('Você tem certeza que quer fechar a janela?'))
+		window.close('_parent');
+	//else (alert('OK, esta janela não será fechada ainda.'))
+}*/
