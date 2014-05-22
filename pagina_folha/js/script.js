5ec87702254e9b1c6618bf32103f4beb92eec778
Nikofoxxx <nicolashenrique2@hotmail.com>
2014-05-22 14:36:58 -0300

    Update18

--- pagina_folha/js/script.js
+++ pagina_folha/js/script.js
@@ -28,14 +28,79 @@ function login(){
 		window.location = '../index.html';
 	//else (alert('OK, esta janela não será fechada ainda.'))
 }
-/*
-<a href="javascript:void();" onclick="login()" id ="item7" class="top_link"><span class="down">Logout</span></a>
-			<ul class="sub">
-			<li><a href="javascript:void();" onclick="fechar()">Fechar</a></li>	
 
-
-function fechar(){
-	if(confirm('Você tem certeza que quer fechar a janela?'))
-		window.close('_parent');
-	//else (alert('OK, esta janela não será fechada ainda.'))
-}*/
+function gerar_folha(){
+ 
+    //dados a enviar, vai buscar os valores dos campos que queremos enviar para a BD
+    var dadosajax = {
+      
+          'competencia' : $("#campo_competencia").val(),     
+      	  'funcionario' : $("#campo_funcionario").val(),
+	      'dias_uteis' : $("#campo_dias_uteis").val(),
+	      'fds' : $("#campo_fds").val(),
+	      'hora_extra_normal' : $("#campo_hora_extra_normal").val(),	      
+	      'hora_extra_fds' : $("#campo_hora_extra_fds").val(),	      
+	      'falta_normal' : $("#campo_falta_normal").val(),
+	      'falta_fds' : $("#campo_falta_fds").val(),
+	      'add_noturno' : $("#campo_add_noturno").val(),
+	      'dependentes' : $("#campo_dependentes").val(),
+	      'adiantamento' : $("#campo_adiantamento").val(),
+    };
+    pageurl = 'php/gerar_folha.php';
+    //para consultar mais opcoes possiveis numa chamada ajax
+    //http://api.jquery.com/jQuery.ajax/
+    $.ajax({
+ 
+        //url da pagina
+        url: pageurl,
+        //parametros a passar
+        data: dadosajax,
+        //tipo: POST ou GET
+        type: 'POST',
+        //cache
+        cache: false,
+        //se ocorrer um erro na chamada ajax, retorna este alerta
+        //possiveis erros: pagina nao existe, erro de codigo na pagina, falha de comunicacao/internet, etc etc etc
+        error: function(){
+            alert('Erro: Inserir Registo!!');
+        },
+        //retorna o resultado da pagina para onde enviamos os dados
+        success: function(result)
+        { 
+            //se foi inserido com sucesso
+            if($.trim(result) != '1')
+            {
+			   alert("Folha Gerada com sucesso!");
+               document.getElementById('campo_competencia').value="";
+               document.getElementById('campo_funcionario').value="";
+               document.getElementById('campo_dias_uteis').value="";
+               document.getElementById('campo_fds').value="";
+               document.getElementById('campo_hora_extra_normal').value="";
+               document.getElementById('campo_hora_extra_fds').value="";
+               document.getElementById('campo_falta_normal').value="";
+               document.getElementById('campo_falta_fds').value="";
+               document.getElementById('campo_add_noturno').value="";
+               document.getElementById('campo_dependentes').value="";
+               document.getElementById('campo_adiantamento').value="";
+            }
+            //se foi um erro
+            else
+            {
+             alert("Não foi possível gerar a folha!");
+               document.getElementById('campo_competencia').value="";
+               document.getElementById('campo_funcionario').value="";
+               document.getElementById('campo_dias_uteis').value="";
+               document.getElementById('campo_fds').value="";
+               document.getElementById('campo_hora_extra_normal').value="";
+               document.getElementById('campo_hora_extra_fds').value="";
+               document.getElementById('campo_falta_normal').value="";
+               document.getElementById('campo_falta_fds').value="";
+               document.getElementById('campo_add_noturno').value="";
+               document.getElementById('campo_dependentes').value="";
+               document.getElementById('campo_adiantamento').value="";
+            }
+ 
+        }
+    });
+}
+ 
---
+++ pagina_folha/php/gerar_folha.php
@@ -0,0 +1,31 @@
+<?php
+$host="localhost";
+$user="root";
+$pass="";
+$banco="payroll_database";
+
+$conexao = mysqli_connect($host, $user, $pass) or die(mysql_error());
+mysqli_select_db($conexao,$banco) or die(mysql_error());
+
+
+$competencia=$_POST['competencia'];
+$funcionario=$_POST['funcionario'];
+$dias_uteis=$_POST['dias_uteis'];
+$fds=$_POST['fds'];
+$hora_extra_normal=$_POST['hora_extra_normal'];
+$hora_extra_fds=$_POST['hora_extra_fds'];
+$falta_normal=$_POST['falta_normal'];
+$falta_fds=$_POST['falta_fds'];
+$add_noturno=$_POST['add_noturno'];
+$dependentes=$_POST['dependentes'];
+$adiantamento=$_POST['adiantamento'];
+
+$sql = mysqli_query ($conexao,"INSERT INTO folha (competencia, funcionario, dias_uteis, dias_fds, hora_normal, hora_fds, falta_normal, falta_fds, add_noturno, 
+	dependentes, adiantamento)
+	VALUES('$competencia', '$funcionario', '$dias_uteis', '$fds', '$hora_extra_normal', '$hora_extra_fds', '$falta_normal', '$falta_fds', '$add_noturno', 
+		'$dependentes', '$adiantamento')") or die(mysql_error());
+
+?>
+
+
+
